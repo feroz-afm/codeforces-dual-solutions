82a866655e631733694dc34c05ebacb383541982
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-01-06 23:53:22 +0600

    Add main function to solve integer difference problem
    Solve CF 1409A - Yet Another Two Integers (C++)

--- cpp/800/1409A_Yet_Another_Two_Integers.cpp
+++ cpp/800/1409A_Yet_Another_Two_Integers.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b;
+        cin >> a >> b;
+
+        long long diff = llabs(a - b);
+        long long moves = (diff + 9) / 10; // ceil(diff / 10)
+
+        cout << moves << '\n';
+    }
+
+    return 0;
+}
