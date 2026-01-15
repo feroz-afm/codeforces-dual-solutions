b49be8e6dbfe2f6c120a2f5490db5c81514db2d1
Md Feroz Ahmed <ferozahmedafm@gmail.com>
2026-01-15 23:06:55 +0600

    Update 1409A_Yet_Another_Two_Integers.py

--- python/800/1409A_Yet_Another_Two_Integers.py
+++ python/800/1409A_Yet_Another_Two_Integers.py
@@ -4,5 +4,5 @@ t = int(input())
 for _ in range(t):
     a, b = map(int, input().split())
     diff = abs(a - b)
-    moves = (diff + 9) // 10   # ceil(diff / 10)
+    moves = (diff + 9) // 10   # (ceil(diff / 10))
     print(moves)
